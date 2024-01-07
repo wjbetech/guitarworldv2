5d9f443b51936d31e35fa0e454a3f057c6750f21
William East <wjbetech@gmail.com>
2024-01-07 16:48:37 +0000

    Added more auth functionality

--- src/libs/sanityClient.ts
+++ src/libs/sanityClient.ts
@@ -1,1 +1,11 @@
 import { createClient } from "next-sanity";
+
+const sanityClient = createClient({
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID as string,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET as string,
+  useCdn: process.env.NODE_ENV === "production",
+  token: process.env.SANITY_EDITOR_TOKEN as string,
+  apiVersion: "2022-03-07"
+})
+
+export default sanityClient;
